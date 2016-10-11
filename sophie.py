fb3df88d32477cd029f211e0522eb74ecd7d30c6
aryamccarthy <arya.mccarthy@gmail.com>
2016-10-10 19:59:40 -0500

    Objectification.

--- sophie.py
+++ sophie.py
@@ -1,28 +1,38 @@
+from __future__ import print_function  # Python 2 compatibility
+from builtins import input  # Python 2 compatibility
 import readline  # For backspace in prompt.
-from .termcolor import colored
 
-PARTNER_NAME = "Sophie"
-
-
-def analyze(user_input: str) -> str:
-    return user_input
+from termcolor import colored
 
 
-def get_input() -> str:
-    prompt = colored("You: ", color='red', attrs={"bold"})
-    return input(prompt)
+class Sophie(object):
+    """A self-contained conversationalist."""
+
+    def __init__(self):
+        super(Sophie, self).__init__()
+        self.NAME = "Sophie"
+        self._prompt = colored("You: ", color='red', attrs={"bold"})
+        self._prefix = colored("{}:".format(self.NAME), 'blue', attrs={"bold"})
 
 
-def print_response(response: str) -> ():
-    prefix = colored("{}:".format(PARTNER_NAME), 'blue', attrs={"bold"})
-    print(prefix, response)
+    def converse(self):
+        while True:
+            remark = self.listen()
+            response = self.analyze(remark)
+            self.speak(response)
+        
+    def listen(self):
+        return input(self._prompt)
+
+    def speak(self, response):
+        print(self._prefix, response)
+
+    def analyze(self, remark):
+        return 'You said "{}"!'.format(remark)
 
 
 def main():
-    while True:
-        user_input = get_input()
-        response = analyze(user_input)
-        print_response(response)
+    Sophie().converse()
 
 if __name__ == '__main__':
     main()
